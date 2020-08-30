9fa8e68945f8ed544be9bcdd50c999eec70a81ac
JY8752 <j.y.8752@gmail.com>
2020-08-30 14:48:19 +0900

    dropdown

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,7 @@
 import '@riotjs/hot-reload'
 import { component } from 'riot'
 import 'semantic-ui-riot'
-import App from './app6.riot'
+import App from './app7.riot'
 
 component(App)(document.getElementById('app'), {
     title: 'Semantic UI Riot Demo'
