d63f43b9e1b2ee2ccafc05d0e1b56e7c4f1d33cc
JY8752 <j.y.8752@gmail.com>
2020-08-30 14:36:14 +0900

    styling 学習中

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,7 @@
 import '@riotjs/hot-reload'
 import { component } from 'riot'
 import 'semantic-ui-riot'
-import App from './app3.riot'
+import App from './app6.riot'
 
 component(App)(document.getElementById('app'), {
     title: 'Semantic UI Riot Demo'
