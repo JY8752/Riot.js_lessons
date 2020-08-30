713e5fca36b672dc155f8a86fff2b94bd0c41ba5
JY8752 <j.y.8752@gmail.com>
2020-08-30 15:03:53 +0900

    modal

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,7 @@
 import '@riotjs/hot-reload'
 import { component } from 'riot'
 import 'semantic-ui-riot'
-import App from './app7.riot'
+import App from './app8.riot'
 
 component(App)(document.getElementById('app'), {
     title: 'Semantic UI Riot Demo'
