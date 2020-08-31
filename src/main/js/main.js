decafb1619ec9b2d2d5ad02d7287085d5276d8d5
JY8752 <j.y.8752@gmail.com>
2020-09-01 00:08:38 +0900

    material design

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,7 @@
 import '@riotjs/hot-reload'
 import { component } from 'riot'
 import 'semantic-ui-riot'
-import App from './app9.riot'
+import App from './app10.riot'
 
 component(App)(document.getElementById('app'), {
     title: 'Semantic UI Riot Demo'
