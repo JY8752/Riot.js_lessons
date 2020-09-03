919e13545526dba79e7fdfe7169036062e6b74c3
JY8752 <j.y.8752@gmail.com>
2020-09-04 08:59:10 +0900

    router導入

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,8 +1,10 @@
 import '@riotjs/hot-reload'
-import { component } from 'riot'
-import 'semantic-ui-riot'
-import App from './app11.riot'
+import { Route, Router } from '@riotjs/route'
+import { component, register } from 'riot'
+import App from './app12.riot'
 
+register('router', Router)
+register('route', Route)
 component(App)(document.getElementById('app'), {
-    title: 'Hello Riot with Ionic'
+    title: 'Riot Route Demo'
 })
